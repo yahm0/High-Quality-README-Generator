6f32e15797d2e6bf19f6dfb53f175573c705a446
yahm0 <stnt190@outlook.com>
2024-04-25 15:55:41 -0400

    Added multiple frames to the index.js

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,3 +1,84 @@
+// Author: Adam Nuccio
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./generateMD.js');
+
+// Array of questions for user input and license choices
+const questions = [
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+    },
+    {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a description of your project:'
+    },
+    {
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide installation instructions:'
+    },
+    {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide usage information:'
+    },
+    {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+    type: 'input',
+    name: 'contributing',
+    message: 'Please provide contribution guidelines:'
+    },
+    {
+    type: 'input',
+    name: 'tests',
+    message: 'Please provide test instructions:'
+    },
+    {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?'
+    },
+    {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+    }
+];
+
+
+// Function to write README file
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdown = generateMarkdown(answers);
+        fs.writeFile('README.md', markdown, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('README.md has been generated!');
+            }
+        });
+    });
+}
+
+// Function call to initialize app
+
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdown = generateMarkdown(answers);
+        fs.writeFile('README.md', markdown, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('README.md has been generated!');
+            }
+        });
+    });
+}
